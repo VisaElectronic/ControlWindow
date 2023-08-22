a11d8c5b642e547989a7e14fcc760eab77044dac
electro42 <visalmathandgamer@gmail.com>
2023-08-22 21:47:02 +0700

    first commit

---
+++ ControlWindow/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Window window;
+        public MainWindow(Window targetWindow)
+        {
+            window = targetWindow;
+            InitializeComponent();
+        }
+
+        private void MoveUp(object sender, EventArgs e)
+        {
+            var moveUp = window.GetType().GetMethod("MoveUp");
+            if(moveUp != null)
+            {
+                moveUp.Invoke(window, new object[0]);
+            }
+        }
+        private void MoveRight(object sender, EventArgs e)
+        {
+            var moveUp = window.GetType().GetMethod("MoveRight");
+            if (moveUp != null)
+            {
+                moveUp.Invoke(window, new object[0]);
+            }
+        }
+        private void MoveLeft(object sender, EventArgs e)
+        {
+            var moveUp = window.GetType().GetMethod("MoveLeft");
+            if (moveUp != null)
+            {
+                moveUp.Invoke(window, new object[0]);
+            }
+        }
+        private void MoveDown(object sender, EventArgs e)
+        {
+            var moveUp = window.GetType().GetMethod("MoveDown");
+            if (moveUp != null)
+            {
+                moveUp.Invoke(window, new object[0]);
+            }
+        }
+    }
+}
---
+++ MoveBox/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.DirectoryServices.ActiveDirectory;
+using System.ComponentModel;
+
+namespace MoveBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ButtonWindow.MainWindow BtnMovingWindow;
+        ControlWindow.MainWindow CTWindow;
+        public MainWindow()
+        {
+            InitializeComponent();
+            BtnMovingWindow = new ButtonWindow.MainWindow();
+            CTWindow = new ControlWindow.MainWindow(BtnMovingWindow);
+            CTWindow.Closing += new CancelEventHandler(MainWindow_Closing);
+            this.Hide();
+            CTWindow.Show();
+            BtnMovingWindow.Show();
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if(BtnMovingWindow != null)
+            {
+                BtnMovingWindow.Close();
+            }
+            Application.Current.MainWindow.Close();
+        }
+    }
+}
